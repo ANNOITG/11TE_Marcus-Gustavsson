2f35c4a119f3dfb4e7c43b0c6c68bed426f702a1
Gulketchup <g_95@live.se>
2014-02-11 11:30:31 +0100

    ProductsStore (ProductStore 1-7)

---
+++ Webbserverprogramering 2/ProductsStore/ProductsStore/Models/OrdersContextInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProductsStore.Models
+{
+    public class OrdersContextInitializer : DropCreateDatabaseIfModelChanges<OrdersContext>
+    {
+        protected override void Seed(OrdersContext context)
+        {
+            var products = new List<Product>()
+            {
+                new Product() { Name = "Tomato", Price = 1.39M, ActualCost = .99M},
+                new Product() { Name = "Hammer", Price = 2.45M, ActualCost = 1.29M},
+                new Product() { Name = "Yo-yo", Price = 12M, ActualCost = 7.12M}
+            };
+
+            products.ForEach(p => context.Products.Add(p));
+            context.SaveChanges();
+
+            var order = new Order() { Customer = "Bob" };
+            var od = new List<OrderDetail>()
+            {
+                new OrderDetail() { Product = products[0], Quantity = 2, Order = order},
+                new OrderDetail() { Product = products[1], Quantity = 4, Order = order}
+            };
+            context.Orders.Add(order);
+            od.ForEach(o => context.OrderDetails.Add(o));
+
+            context.SaveChanges();
+        }
+    }
+}
